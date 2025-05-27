5c715726fccf62cbc805b6d54382ebf68b28072f
Eric Furufors <erifu944@student.liu.se>
2025-05-27 23:35:25 +0200

    Removes the zip file after install

--- portableinstall.sh
+++ portableinstall.sh
@@ -1,4 +1,5 @@
 wget -q https://github.com/neovim/neovim/releases/download/nightly/nvim-linux-x86_64.tar.gz
 tar xzvf nvim-linux-x86_64.tar.gz
 ln -s nvim-linux-x86_64/bin/nvim nvim
+rm nvim-linux-x86_64.tar.gx
 ./nvim
