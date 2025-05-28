ae2a8591e995dd59c8b442c54c4846c1eb105d0f
Efuri <33378439+Efuri@users.noreply.github.com>
2025-05-29 01:15:30 +0200

    Update installnvim.sh

--- installnvim.sh
+++ installnvim.sh
@@ -1,5 +1,5 @@
 sudo apt update
-sudo apt install ninja-build gettext cmake unzip curl build-essential git
+sudo apt install ninja-build gettext cmake unzip curl build-essential git npm
 git clone https://github.com/neovim/neovim.git
 cd neovim
 git checkout master
