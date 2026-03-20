1ebce06f66a38972716343d2a3bbfb471baf88b0
Eric <eric@eric.ad>
2026-03-20 17:44:25 +0100

    installall

---
+++ installall.sh
@@ -0,0 +1,3 @@
+sh installnvim.sh
+sh installessentials.sh
+sh installlatex.sh
--- installessentials.sh
+++ installessentials.sh
@@ -2,4 +2,17 @@ sudo apt install tmux ripgrep fd-find nodejs npm unzip build-essential xclip
 echo "set -g base-index 1" >> ~/.tmux.conf
 echo "set -g pane-base-index 1" >> ~/.tmux.conf
 echo "set -s escape-time 0" >> ~/.tmux.conf
+echo "export NVIM_LOG_LEVEL=info" >> ~/.bashrc
+# Function to ensure a line exists in a file
+ensure_line() {
+    local line="$1"
+    local file="$2"
+    
+    touch "$file"
+    grep -qF -- "$line" "$file" || echo "$line" >> "$file"
+}
 
+ensure_line "set -g base-index 1" ~/.tmux.conf
+ensure_line "set -g pane-base-index 1" ~/.tmux.conf
+ensure_line "set -s escape-time 0" ~/.tmux.conf
+ensure_line "export NVIM_LOG_LEVEL=info" ~/.bashrc
